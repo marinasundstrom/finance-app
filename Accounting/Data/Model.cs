45506324842d3e90108d1a63472a49d566b50298
Robert Sundström <robert.sundstrom@outlook.com>
2021-10-09 23:08:50 +0200

    Add more accounts and update seed

--- Accounting/Data/Model.cs
+++ Accounting/Data/Model.cs
@@ -25,107 +25,249 @@ public class AccountingContext : DbContext
 
 public static class AccountingContextSeed
 {
+    public static bool RegenerateDatabase { get; set; } = false;
+
+    public static bool SeedAccounts { get; set; } = false;
+
+    public static bool SeedVerifications { get; set; } = false;
+
     public static async void SeedAsync(this IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
         using var context = scope.ServiceProvider.GetRequiredService<AccountingContext>();
 
-        await context.Database.EnsureDeletedAsync();
-        await context.Database.EnsureCreatedAsync();
+        if (RegenerateDatabase)
+        {
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+        }
 
-        context.Accounts.AddRange(
-            new Account
-            {
-                AccountNo = 1510,
-                Name = "Kundfordringar",
-                Description = String.Empty
-            },
-            new Account
-            {
-                AccountNo = 3051,
-                Name = "Försäljning varor 25% moms Sverige",
-                Description = String.Empty
-            },
-            new Account
-            {
-                AccountNo = 3041,
-                Name = "Försäljning tjänster 25% moms Sverige",
-                Description = String.Empty
-            },
-            new Account
-            {
-                AccountNo = 2611,
-                Name = "Utgående moms Försäljning Sverige 25%",
-                Description = String.Empty
-            },
-            new Account
-            {
-                AccountNo = 1930,
-                Name = "Bank",
-                Description = String.Empty
-            });
+        if (SeedAccounts)
+        {
 
-        context.Verifications.AddRange(
-            new Verification
-            {
-                VerificationNo = "V1",
-                Date = DateTime.Now.Subtract(TimeSpan.FromDays(19)),
-                Description = string.Empty,
-                Attachment = String.Empty
-            },
-            new Verification
-            {
-                VerificationNo = "V2",
-                Date = DateTime.Now,
-                Description = string.Empty,
-                Attachment = String.Empty
-            });
+            context.Accounts.AddRange(
+                new Account
+                {
+                    AccountNo = 1510,
+                    Name = "Kundfordringar",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 3051,
+                    Name = "Försäljning varor 25% moms Sverige",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 3041,
+                    Name = "Försäljning tjänster 25% moms Sverige",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 2611,
+                    Name = "Utgående moms Försäljning Sverige 25%",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 1930,
+                    Name = "Företagskonto",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 5010,
+                    Name = "Lokalhyra",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 5220,
+                    Name = "Hyra av inventarier och verktyg",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 5410,
+                    Name = "Förbrukningsinventarier",
+                    Description = "Inköp av inventarier som kostar mindre än ett halvt prisbasbelopp*, t.ex. dator, skrivare, kontorsstol"
+                },
+                new Account
+                {
+                    AccountNo = 5460,
+                    Name = "Förbrukningsmaterial",
+                    Description = "Material som förbrukas inom företaget, t.ex. spik, skruv"
+                },
+                new Account
+                {
+                    AccountNo = 5480,
+                    Name = "Arbetskläder & skyddsmaterial",
+                    Description = "Arbetskläder, Terminalglasögon Läs mer på Skatteverkets sida"
+                },
+                new Account
+                {
+                    AccountNo = 5910,
+                    Name = "Annonsering",
+                    Description = "Annonser jag köper inom Sverige"
+                },
+                new Account
+                {
+                    AccountNo = 5912,
+                    Name = "Annonsering EU",
+                    Description = "Annonser jag köper från företag in EU, t.ex. Google, Facebook när de fakturerar från Irland"
+                },
+                new Account
+                {
+                    AccountNo = 6212,
+                    Name = "Mobiltelefon",
+                    Description = "Telefonräkningen"
+                },
+                new Account
+                {
+                    AccountNo = 6540,
+                    Name = "IT-tjänster",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 6541,
+                    Name = "Redovisningsprogram",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 6570,
+                    Name = "Banktjänster",
+                    Description = "Alla tjänster och avgifter till din bank. Viktigt att tänka på att banktjänster är momsfria"
+                },
+                new Account
+                {
+                    AccountNo = 6910,
+                    Name = " Licensavgifter och royalties",
+                    Description = "Royalty och licensavgifter"
+                },
+                new Account
+                {
+                    AccountNo = 6970,
+                    Name = "Tidningar, facklitteratur",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 1910,
+                    Name = "Kassa",
+                    Description = "Insättningar av kontanter från kassan"
+                },
+                new Account
+                {
+                    AccountNo = 1940,
+                    Name = "Placeringskonto",
+                    Description = "Överföringar till och från placeringskonto/sparkonto"
+                },
+                new Account
+                {
+                    AccountNo = 2013,
+                    Name = "Egna Uttag",
+                    Description = "Privata uttag \"lön\" i enskild firma"
+                },
+                new Account
+                {
+                    AccountNo = 2018,
+                    Name = "Egna Insättningar",
+                    Description = "Privata insättningar i enskild firma"
+                },
+                new Account
+                {
+                    AccountNo = 2650,
+                    Name = "Redovisningskonto för moms",
+                    Description = "Får tillbaka eller betalar moms till skatteverket"
+                },
+                new Account
+                {
+                    AccountNo = 3740,
+                    Name = "Öresavrundning",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 8310,
+                    Name = "Ränteintäkter",
+                    Description = String.Empty
+                },
+                new Account
+                {
+                    AccountNo = 8410,
+                    Name = "Räntekostnader",
+                    Description = String.Empty
+                });
+        }
 
-        context.Entries.AddRange(
-            new Entry
-            {
-                AccountNo = 1510,
-                VerificationNo = "V1",
-                Description = string.Empty,
-                Debit = 6500m
-            },
-            new Entry
-            {
-                AccountNo = 3051,
-                VerificationNo = "V1",
-                Description = string.Empty,
-                Credit = 4800m
-            },
-            new Entry
-            {
-                AccountNo = 3041,
-                VerificationNo = "V1",
-                Description = string.Empty,
-                Credit = 400m
-            },
-            new Entry
-            {
-                AccountNo = 2611,
-                VerificationNo = "V1",
-                Description = string.Empty,
-                Credit = 1300m
-            });
+        if (SeedVerifications)
+        {
+            context.Verifications.AddRange(
+                new Verification
+                {
+                    VerificationNo = "V1",
+                    Date = DateTime.Now.Subtract(TimeSpan.FromDays(19)),
+                    Description = string.Empty,
+                    Attachment = String.Empty
+                },
+                new Verification
+                {
+                    VerificationNo = "V2",
+                    Date = DateTime.Now,
+                    Description = string.Empty,
+                    Attachment = String.Empty
+                });
 
-        context.Entries.AddRange(
-           new Entry
-           {
-               AccountNo = 1510,
-               VerificationNo = "V2",
-               Description = string.Empty,
-               Credit = 6500m
-           },
-           new Entry
-           {
-               AccountNo = 1930,
-               VerificationNo = "V2",
-               Description = string.Empty,
-               Debit = 6500m
-           });
+            context.Entries.AddRange(
+                new Entry
+                {
+                    AccountNo = 1510,
+                    VerificationNo = "V1",
+                    Description = string.Empty,
+                    Debit = 6500m
+                },
+                new Entry
+                {
+                    AccountNo = 3051,
+                    VerificationNo = "V1",
+                    Description = string.Empty,
+                    Credit = 4800m
+                },
+                new Entry
+                {
+                    AccountNo = 3041,
+                    VerificationNo = "V1",
+                    Description = string.Empty,
+                    Credit = 400m
+                },
+                new Entry
+                {
+                    AccountNo = 2611,
+                    VerificationNo = "V1",
+                    Description = string.Empty,
+                    Credit = 1300m
+                });
+
+            context.Entries.AddRange(
+               new Entry
+               {
+                   AccountNo = 1510,
+                   VerificationNo = "V2",
+                   Description = string.Empty,
+                   Credit = 6500m
+               },
+               new Entry
+               {
+                   AccountNo = 1930,
+                   VerificationNo = "V2",
+                   Description = string.Empty,
+                   Debit = 6500m
+               });
+        }
 
         await context.SaveChangesAsync();
     }
