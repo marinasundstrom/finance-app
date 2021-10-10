ddea5a6e2b312cd1e5b445691fedfea8760ad83b
Robert Sundström <robert.sundstrom@outlook.com>
2021-10-10 16:52:15 +0200

    Data

---
+++ Accounting/Data/AccountingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Accounting.Data;
+
+public class AccountingContext : DbContext
+{
+    public AccountingContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+#nullable disable
+
+    public DbSet<Account> Accounts { get; set; }
+
+    public DbSet<Verification> Verifications { get; set; }
+
+    public DbSet<Entry> Entries { get; set; }
+
+#nullable restore
+}
